66d7e92e683dfe4e20bbea54b12c8c91b6e15f7f
vanya642004 <139208978+vanya642004@users.noreply.github.com>
2025-07-22 15:19:37 +0530

    Update db_loader.py

--- db_loader.py
+++ db_loader.py
@@ -4,9 +4,8 @@ import sqlite3
 def load_all_csvs():
     conn = sqlite3.connect(":memory:")
 
-    pd.read_csv("ad_sales.csv").to_sql("ad_sales", conn, index=False, if_exists="replace")
-    pd.read_csv("total_sales.csv").to_sql("total_sales", conn, index=False, if_exists="replace")
-    pd.read_csv("eligibility.csv").to_sql("eligibility", conn, index=False, if_exists="replace")
+    pd.read_csv("ecommerce_sql_agent/ad_sales.csv").to_sql("ad_sales", conn, index=False, if_exists="replace")
+    pd.read_csv("ecommerce_sql_agent/total_sales.csv").to_sql("total_sales", conn, index=False, if_exists="replace")
+    pd.read_csv("ecommerce_sql_agent/eligibility.csv").to_sql("eligibility", conn, index=False, if_exists="replace")
 
     return conn
-
