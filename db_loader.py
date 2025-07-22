3492128547de0ae65196353aeceb0f412d489fab
vanya642004 <139208978+vanya642004@users.noreply.github.com>
2025-07-22 15:07:41 +0530

    Update db_loader.py

--- db_loader.py
+++ db_loader.py
@@ -1,15 +1,12 @@
-import duckdb
 import pandas as pd
+import sqlite3
 
-def load_data():
-    con = duckdb.connect(database='ecom.duckdb', read_only=False)
+def load_all_csvs():
+    conn = sqlite3.connect(":memory:")
 
-    ad = pd.read_csv('ad_sales.csv')
-    total = pd.read_csv('total_sales.csv')
-    eligibility = pd.read_csv('eligibility.csv')
+    pd.read_csv("ad_sales.csv").to_sql("ad_sales", conn, index=False, if_exists="replace")
+    pd.read_csv("total_sales.csv").to_sql("total_sales", conn, index=False, if_exists="replace")
+    pd.read_csv("eligibility.csv").to_sql("eligibility", conn, index=False, if_exists="replace")
 
-    con.execute("CREATE OR REPLACE TABLE ad_sales AS SELECT * FROM ad")
-    con.execute("CREATE OR REPLACE TABLE total_sales AS SELECT * FROM total")
-    con.execute("CREATE OR REPLACE TABLE eligibility AS SELECT * FROM eligibility")
+    return conn
 
-    return con
